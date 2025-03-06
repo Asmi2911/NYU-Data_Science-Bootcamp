45deedbe73da3010f8d31280483d718c6319232a
Asmita <asmitass2911@gmail.com>
2025-03-05 19:53:25 -0500

    Create 6_odd_even.py

---
+++ week-1/6_odd_even.py
@@ -0,0 +1,9 @@
+def check_odd_even():
+    for num in range(1, 21):  # Loop from 1 to 20
+        if num % 2 == 0:
+            print(f"{num} is Even")
+        else:
+            print(f"{num} is Odd")
+
+# Call the function
+check_odd_even()
